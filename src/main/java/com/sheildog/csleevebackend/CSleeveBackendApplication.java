ca1e65210ae98b9b14715a2fa706486a16939953
lhsheildg <lhsheild@yahoo.com>
2020-08-31 21:17:35 +0800

    Initial commit

---
+++ src/main/java/com/sheildog/csleevebackend/CSleeveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sheildog.csleevebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CSleeveBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CSleeveBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sheildog/csleevebackend/CSleeveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheildog.csleevebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CSleeveBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
