8f02fe950677993207b75a4fcff5eb5efc1764ba
lhsheildg <lhsheild@yahoo.com>
2020-09-05 01:16:02 +0800

    lombok

--- src/main/java/com/sheildog/csleevebackend/api/v1/BannerController.java
+++ src/main/java/com/sheildog/csleevebackend/api/v1/BannerController.java
@@ -1,12 +1,11 @@
 package com.sheildog.csleevebackend.api.v1;
 
+import com.sheildog.csleevebackend.dto.PersonDTO;
 import com.sheildog.csleevebackend.exception.http.ForbiddenException;
 import com.sheildog.csleevebackend.exception.http.NotFoundException;
 import com.sheildog.csleevebackend.sample.hero.ISkill;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author a7818
@@ -17,8 +16,8 @@ public class BannerController {
     @Autowired
     private ISkill iSkill;
 
-    @GetMapping("/test")
-    public String test() throws Exception {
+    @PostMapping("/test/{id}/{pid}")
+    public String test(@PathVariable String id, @PathVariable String pid, @RequestBody PersonDTO person) throws Exception {
         iSkill.r();
         throw new ForbiddenException(10001);
     }
---
+++ src/main/java/com/sheildog/csleevebackend/dto/PersonDTO.java
@@ -0,0 +1,11 @@
+package com.sheildog.csleevebackend.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+public class PersonDTO {
+    private String name;
+    private Integer age;
+}
