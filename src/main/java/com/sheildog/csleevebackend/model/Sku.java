5fe72ecab71f9a7a757775405677b587a35ab76c
lhsheildg <lhsheild@yahoo.com>
2020-09-17 00:37:36 +0800

    泛型解决序列化和反序列化

--- src/main/java/com/sheildog/csleevebackend/model/Sku.java
+++ src/main/java/com/sheildog/csleevebackend/model/Sku.java
@@ -1,6 +1,8 @@
 package com.sheildog.csleevebackend.model;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.mysql.cj.x.protobuf.MysqlxDatatypes;
+import com.sheildog.csleevebackend.util.GenericAndJson;
 import com.sheildog.csleevebackend.util.ListAndJson;
 import com.sheildog.csleevebackend.util.MapAndJson;
 import lombok.Getter;
@@ -8,6 +10,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -28,11 +31,28 @@ public class Sku extends BaseEntity {
     private Long spuId;
     private Long categoryId;
     private Long rootCategoryId;
-//    @Convert(converter = MapAndJson.class)
-//    private Map<String, Object> test;
-//    @Convert(converter = ListAndJson.class)
-//    private List<Object> specs;
+    //    @Convert(converter = MapAndJson.class)
+    //    private Map<String, Object> test;
+    //    @Convert(converter = ListAndJson.class)
+    //    private List<Object> specs;
     private String specs;
+    //    private String specs;
     private String code;
     private Long stock;
+
+    public List<Spec> getSpecs() {
+        if (this.specs == null){
+            return Collections.emptyList();
+        }
+//        return GenericAndJson.jsonToList(this.specs, new TypeReference<List<Spec>>(){
+        return GenericAndJson.jsonToList(this.specs);
+//        return specs;
+    }
+
+    public void setSpecs(List<Spec> specs) {
+        if (specs.isEmpty()){
+            return;
+        }
+        this.specs = GenericAndJson.objectToJson(specs);
+    }
 }
---
+++ src/main/java/com/sheildog/csleevebackend/util/GenericAndJson.java
@@ -0,0 +1,73 @@
+package com.sheildog.csleevebackend.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sheildog.csleevebackend.exception.http.ServerErrorException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class GenericAndJson {
+
+    private static ObjectMapper mapper;
+
+    @Autowired
+    public void setMapper(ObjectMapper mapper) {
+        GenericAndJson.mapper = mapper;
+    }
+
+    public static <T> String objectToJson(T o) {
+        try {
+            if (o == null) {
+                return null;
+            }
+            return GenericAndJson.mapper.writeValueAsString(o);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ServerErrorException(9999);
+        }
+    }
+
+    public static <T> T jsonToObject(String s, Class<T> tClass) {
+        if (s == null) {
+            return null;
+        }
+        try {
+            T o = GenericAndJson.mapper.readValue(s, tClass);
+            return o;
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            throw new ServerErrorException(9999);
+        }
+    }
+
+    public static <T> T jsonToList(String s, TypeReference<T> tr) {
+        if (s == null) {
+            return null;
+        }
+        try {
+            T list = GenericAndJson.mapper.readValue(s, tr);
+            return list;
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            throw new ServerErrorException(9999);
+        }
+    }
+
+    public static <T> List<T> jsonToList(String s) {
+        if (s == null) {
+            return null;
+        }
+        try {
+            List<T> list = GenericAndJson.mapper.readValue(s, new TypeReference<List<T>>() {
+            });
+            return list;
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            throw new ServerErrorException(9999);
+        }
+    }
+}
