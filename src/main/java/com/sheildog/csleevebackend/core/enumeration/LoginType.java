1cb64f81ed2670d1835369ef427bb0cbe65e7560
lhsheildg <lhsheild@yahoo.com>
2020-10-09 00:31:06 +0800

    枚举类型

--- src/main/java/com/sheildog/csleevebackend/core/enumeration/LoginType.java
+++ src/main/java/com/sheildog/csleevebackend/core/enumeration/LoginType.java
@@ -1,5 +1,14 @@
 package com.sheildog.csleevebackend.core.enumeration;
 
 public enum LoginType {
-    USER_WX, USER_EMAIL
+    USER_WX(0, "微信登陆"),
+    USER_EMAIL(1, "邮箱登录");
+
+    private Integer value;
+    private String description;
+
+    private LoginType(Integer value, String description) {
+        this.value = value;
+        this.description = description;
+    }
 }
